d84e3d0835f29e684f6a3cb92af731f0382398be
Jackbringas <102544709+Jackbringas@users.noreply.github.com>
2025-02-06 00:31:19 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ['the', 'our', 'my'];
+let adj = ['great', 'big','pretty'];
+let noun = ['jogger', 'racoon', 'star'];
+
+let domainNames = [];
+
+for (let p of pronoun) {
+for (let a of adj) {
+for (let n of noun) {
+
+  let domain = p + a + n + '.com';
+      domainNames.push(domain); 
+    }
+  }
+}
+  console.log(domainNames);
 };
